4451c1bc96715f997dc7fa826df0670f2777fdf5
VL <vlcastroverde794@gmail.com>
2025-12-12 19:25:02 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <h1 className="text-3xl font-bold">ASUZA</h1>
+    </div>
+  )
+}
